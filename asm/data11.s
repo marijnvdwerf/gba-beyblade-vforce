33f916be6477042d9f61dc6742f2119cb084d6b0
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2017-06-03 15:11:53 +0200

    Integrate all data

--- asm/data11.s
+++ asm/data11.s
@@ -2,4 +2,20 @@
 
 	.section .rodata
 
-    .incbin "baserom.gba", 0x685e0, 0x8074da8 - 0x80685e0
+Unk_8074D3C:
+    .4byte Unk_8071B54
+    .4byte Unk_806F934
+    .4byte Unk_8072950
+    .4byte Unk_8073720
+    .4byte Unk_8074184
+    .4byte Unk_8074D14
+    .4byte Unk_806FFE8
+    .4byte Unk_80708D4
+    .4byte Unk_8071134
+    .4byte Unk_8071B54
+
+word_8074D64:
+    .2byte 4, 1, -2, 6, 5, -10, 0xD, 7, 0, -15, 0xE
+    .2byte 9, 3, 1, -8, -3, 7, -9, -10, 0xB, -4
+    .2byte 9, 0xE, 3, -10, 0xD, 0xB, -4, 7, -9, 7
+    .2byte -6, 9, 0xE
