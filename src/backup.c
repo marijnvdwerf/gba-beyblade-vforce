f97a65c5f06793c7cfbfb3be5a3cffd94af3a3ca
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2026-09-07 14:01:26 +0200

    Remove redundant battery pointer cast

--- src/backup.c
+++ src/backup.c
@@ -247,7 +247,7 @@ u16 sub_8065BD4(u16 sector, unk16* data)
     while (retries <= 2) {
         result = sub_8065AA0(sector, data);
         if (result == 0) {
-            result = writeToBatteryBackup(sector, (unk16*)data);
+            result = writeToBatteryBackup(sector, data);
         }
         if (result == 0) {
             break;
