8571ca8b6a88b0d986dcd53090228770f545c698
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2026-09-07 10:38:05 +0200

    Decompile sub_804A280

--- src/levelrow.c
+++ src/levelrow.c
@@ -73,26 +73,24 @@ INCLUDE_ASM("asm/dump/8040d18/804a1e4.s");
 INCLUDE_ASM("asm/dump/8040d18/804a234.s");
 INCLUDE_ASM("asm/dump/8040d18/804a26c.s");
 
-#if 0
 void sub_804A280(FrontendState* arg0)
 {
-    unk32 current;
-    unk32 index;
+    s32 index;
     const FrontendSelectionRecord* entry;
 
-    current = arg0->unk0;
+    if (arg0->unk0 == arg0->unk4)
+        return;
+
     index = arg0->unk4;
-    if (current != index) {
-        entry = &_8069FC8[index];
-        if (entry->data->unk0 != _gameData->unkC26 && arg0->transition.unk586 == arg0->transition.value) {
-            if (_gameData->unkC26 != -1) {
-                sub_804AF5C();
-            }
-            if (index != -1 && entry->data->unk0 != -1) {
-                sub_804AF04(entry->data->unk0);
-            }
-        }
-    }
+    entry = &_8069FC8[index];
+
+    if (entry->data->unk0 == (s16)_gameData->unkC26)
+        return;
+    if (arg0->transition.unk586 != arg0->transition.value)
+        return;
+
+    if ((s16)_gameData->unkC26 != -1)
+        sub_804AF5C();
+    if (index != -1 && entry->data->unk0 != -1)
+        sub_804AF04(entry->data->unk0);
 }
-#endif
-INCLUDE_ASM("asm/dump/8040d18/804a280.s");
