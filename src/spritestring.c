8582eb95d5e5c5add7ea84e4fcaeec8202517789
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2026-09-18 11:20:36 +0200

    spritestring: drop the cast on an inlined string literal

--- src/spritestring.c
+++ src/spritestring.c
@@ -108,7 +108,7 @@ SpriteStringActorBlock* SpriteString_8064FE8(SpriteString* string, const u8* arg
 
     state = sub_8062EFC(count);
     if (state == NULL) {
-        nullsub_8((const unk8*)"Error allocating actors for SpriteString");
+        nullsub_8("Error allocating actors for SpriteString");
     }
     i = 0;
     if (i < count) {
